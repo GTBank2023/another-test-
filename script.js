a99f086222fb4b672d2056de54afb23b4821a703
GTBank2023 <140178184+GTBank2023@users.noreply.github.com>
2023-09-20 11:35:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -191,8 +191,8 @@ function initializeDetectedAreas() {
 // Call this function to initialize the array
 initializeDetectedAreas();
 
-import tf from '@tensorflow/tfjs';
-        
+const tf = require('@tensorflow/tfjs');
+     
 document.addEventListener('DOMContentLoaded', async () => {
     const videoElement = document.getElementById('video-feed');
     const canvas = document.getElementById('canvas');
